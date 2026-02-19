7ba441513dc569576644aa48f811eab6fd419d8a
Jennifer <jennifer.m.gao@gmail.com>
2026-02-19 00:04:04 -0800

    added home page text and font

--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style = {{padding: '40px'}}>
-      KKDS Home page!
+    <div className="p-10 bg-[#FFE299] text-black">
+      <h1 className="text-[#830033] font-[Oleo_Script_Swash_Caps] text-5xl">Kalamandapam</h1>
+      <h2>Kuchipudi Dance School</h2>
+      <p>Discover the vibrant world of Kuchipudi dance at our studio in Sammamish, Washington</p>
+      <button className="bg-[#830033] text-white">Learn More</button>
     </div>
   );
 }
