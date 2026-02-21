31fe23cf215d2fb638a356b335bac6c40fe7017a
Jennifer <jennifer.m.gao@gmail.com>
2026-02-21 09:25:45 -0800

    added header component and styled landing page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Button from "../components/Button"
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -15,70 +16,6 @@ export default function Home() {
         </div>
         <div className="absolute top-153.25">
           <Button>Learn More</Button>
-        </div>
-      </div>
-
-      {/* message */}
-      <div className="flex justify-center items-center p-10 gap-2.5 mx-auto">
-        <p className="text-center text-[32px] leading-12">
-          At Kalamandapam, we believe in the power of dance to inspire, educate, and connect people. Our experienced
-          instructors are passionate about sharing their knowledge and expertise, ensuring that each student
-          recieves personalized attention to develop their skills, grace, and confidence. Through a blend of traditional
-          techniques and modern teaching methods, we create a dyanmic learning experience that celebrate the heritage of Kuchipudi
-          while encouraging individual expression.
-        </p>
-      </div>
-
-      {/* classes offered */}
-      <div className="h-279.25 bg-[#FFE299] p-20 g-2.5 text-center">
-        <div className="w-319.75 g-[80px]">
-          <div className="h-17.5 mb-10">
-            <h2 className="text-[64px] font-bold">Classes Offered</h2>
-          </div>
-          <div className="flex justify-between h-159.75">
-            <div className="w-84.25">
-              <div className="h-82.25 bg-white rounded-t-[10px]"></div>
-              <div className="p-2.5 gap-2.5 h-14.75 bg-[#830033]">
-                <p className="text-[32px] font-bold text-white">Beginner</p>
-              </div>
-              <div className="bg-[#FFFAEE] p-6 g-2.5 rounded-b-[10px] h-62.75">
-                <p className="text-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, 
-                  consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem 
-                  ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-              </div>
-            </div>
-
-            <div className="w-84.25">
-              <div className="h-82.25 bg-white rounded-t-[10px]"></div>
-              <div className="p-2.5 gap-2.5 h-14.75 bg-[#830033]">
-                <p className="text-[32px] font-bold text-white">Intermediate</p>
-              </div>
-              <div className="bg-[#FFFAEE] p-6 g-2.5 rounded-b-[10px] h-62.75">
-                <p className="text-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, 
-                  consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem 
-                  ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-              </div>
-            </div>
-
-            <div className="w-84.25">
-              <div className="h-82.25 bg-white rounded-t-[10px]"></div>
-              <div className="p-2.5 gap-2.5 h-14.75 bg-[#830033]">
-                <p className="text-[32px] font-bold text-white">Advanced</p>
-              </div>
-              <div className="bg-[#FFFAEE] p-6 g-2.5 rounded-b-[10px] h-62.75">
-                <p className="text-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, 
-                  consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem 
-                  ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <Button>Enroll Now</Button>
         </div>
       </div>
     </div>
